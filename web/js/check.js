f117714d5a0fea153c6dc6ef68d72cef1b13091a
sindhya <sindhyakumari90@gmail.com>
2016-12-01 13:41:25 -0800

    login and registration added

--- web/js/check.js
+++ web/js/check.js
@@ -1,11 +1,108 @@
 /**
- * 
+ *
  */
- 
-    function check()
+function check(userid,pswrd,type)
+{
+    if(type === "users")
     {
-    var url = "http://www.google.com";
-    window.location(url);
+        checkusers(userid,pswrd)
     }
-    
- 
+    else if (type === "vendors")
+    {
+        checkvendors(userid,pswrd)
+    }
+}
+
+function checkusers(userid,pswrd) {
+
+    var hash = {};
+
+    jQuery.ajax({
+
+
+        url:"http://localhost:8181/users",
+        type:"GET",
+        contentType:'application/json',
+        dataType:"json",
+        success:function(data) {
+
+            $("#user-table tbody tr").remove();
+            $.each(data, function (i, item) {
+
+
+                hash[item.userName] = item.userPassword
+
+            });
+
+            if(hash[userid] == pswrd)
+            {
+
+                fun2(userid)
+                window.open("users.html")
+            }
+            else
+            {
+                alert("invalid user name and password")
+            }
+        },
+
+    });
+    //alert(hash[item.userName])
+}
+
+function checkvendors(userid,pswrd) {
+
+    var hash = {};
+
+    jQuery.ajax({
+
+
+        url:"http://localhost:8181/vendors",
+        type:"GET",
+        contentType:'application/json',
+        dataType:"json",
+        success:function(data) {
+
+            $("#user-table tbody tr").remove();
+            $.each(data, function (i, item) {
+
+
+                hash[item.vendorName] = item.vendorPassword
+
+            });
+
+            if(hash[userid] == pswrd)
+            {
+
+                fun2(userid)
+                window.open("vendor.html")
+            }
+            else
+            {
+                alert(hash[userid])
+
+                alert("invalid user name and password")
+            }
+        },
+
+    });
+    //alert(hash[item.userName])
+}
+
+function fun2(userid)
+{
+    alert(userid)
+    $.ajax({
+
+        type:'POST',
+        url:'ex.php',
+
+        data:{userid : userid},
+
+        success: function (data) {
+
+            alert(data);
+        }
+    });
+}
+
--- web/js/user-sensor-group.js
+++ web/js/user-sensor-group.js
@@ -2,6 +2,20 @@
  * Created by sindhya on 11/29/16.
  */
 
+function sortByProperty(sensorGrpId) {
+    'use strict';
+    return function (a, b) {
+        var sortStatus = 0;
+        if (a[sensorGrpId] < b[sensorGrpId]) {
+            sortStatus = -1;
+        } else if (a[sensorGrpId] > b[sensorGrpId]) {
+            sortStatus = 1;
+        }
+
+        return sortStatus;
+    };
+}
+
 function user_sensor_group() {
 
     jQuery.ajax({
@@ -11,21 +25,95 @@ function user_sensor_group() {
         contentType:'application/json',
         dataType:"json",
         success:function(data) {
-            console.log(data);
-            var i=1;
-            //data.sort(sortByProperty('sensorGroupId'));
+
+            data.sort(sortByProperty('sensorGroupId'));
             $("#user-sensorGroup-table tbody tr").remove();
             $.each(data, function (i, item) {
 
-                //var $d=$('<button class="btn-sm btn-info btn-fill btn-danger pull-right" data-id="'+item.sensorGroupId.toString()+'" toggle="modal" onclick="deleteSensor(event)">Unsubscribe</button>');
-                //var $e=$('<button class="btn-xs btn-info btn-fill btn-edit btn-pace-left " data-id="'+item.virtualSensorId.toString()+'" data-name="'+item.virtualSensorName+'"  toggle="modal" onclick="editSensor(event)" >Edit</button>');
+                var $l=$('<button class="btn-xs btn-info btn-fill pull-right" data-id="'+item.sensorGroupId.toString()+'" data-name="'+item.sensorGroupName+'" data-list="'+item.virtualSensorId+'" toggle="modal" onclick="listSensors(event)">List Sensors</button>');
+                var $d=$('<button class="btn-xs btn-info btn-fill btn-danger pull-right" data-id="'+item.sensorGroupId.toString()+'" toggle="modal" onclick="deleteGroup(event)" >Delete Group</button>');
 
 
                 //$("#user-virtualsensor-table").append($('<tr/>').append($('<td/>').append(item.virtualSensorId)).append($('<td/>').append(item.sensorName)).append($('<td/>').append(item.sensorLocation)).append($('<td/>').append(item.sensorStatus)).append($('<td/>').append(item.sensorType)).append($('<td/>').append(item.vendorName)).append($('<input type=button onclick="location.href=`edit_user.html`" value= "Delete" class="btn btn-default btn-fill btn-sm" data-toggle="modal" data-target="#edit-users"></button>')));
-                $("#user-sensorGroup-table").append($('<tr/>').append($('<td/>').append(item.sensorGroupId)).append($('<td/>').append(item.sensorGroupName)).append($('<td/>').append(item.sensorGroupDescription)));//.append($('<td/>').append($d)));
-                i++;
+                $("#user-sensorGroup-table").append($('<tr/>').append($('<td/>').append(item.sensorGroupId)).append($('<td/>').append(item.sensorGroupName)).append($('<td/>').append(item.sensorGroupDescription)).append($('<td/>').append($l)).append($('<td/>').append($d)));
+
             });
         },
     });
+
+}
+
+function deleteGroup(event){
+    var id=event.target.dataset.id;
+
+    var delete_endpoint= "http://localhost:8181/virtualSensorGroup/"+id;
+    jQuery.ajax({
+        url: delete_endpoint,
+        type:"DELETE",
+        contentType: 'application/json',
+        //data: JSON.stringify(a),
+        success: function(data,textStatus,xhr){
+
+            //alert(xhr.status);
+            if(xhr.status==204){
+                alert("Sensor Group deleted");
+                //showDeleteDialog(1);
+            }else{
+                //showDeleteDialog(0);
+            }
+
+            window.onload = Refresh;
+            function Refresh() {
+                setTimeout("refreshPage();", 1000);
+            }
+            function refreshPage() {
+                window.location = location.href;
+            }
+        }
+    });
+
+
+}
+
+function listSensors(event){
+
+    var id=event.target.dataset.id;
+    var name=event.target.dataset.name;
+    var sensor_list=event.target.dataset.list;
+    var sensorArray=sensor_list.split(',');
+
+
+    $("#list-sensor-modal").find('.modal-header h4').text(name);
+
+    jQuery.ajax({
+        url: "http://localhost:8181/virtualsensor",
+        //url:"http://localhost:8080/sensor_service/virtualsensor?user_email=archana@sjsu.edu",
+        type: "GET",
+        contentType: 'application/json',
+        dataType: "json",
+        success: function (data) {
+            var status;
+            $("#list-sensor-table tbody tr").remove();
+            $.each(data, function (i, item) {
+                for(var i=0;i<sensorArray.length;i++)
+                {
+                    if(item.virtualSensorId==sensorArray[i]) {
+
+                        if(item.sensorStatus==1){
+                            status = "on";
+                        }
+                        else{
+                            status="off";
+                        }
+
+                        $("#list-sensor-table").append($('<tr/>').append($('<td/>').append(i + 1)).append($('<td/>').append(item.virtualSensorName)).append($('<td/>').append(item.sensorType)).append($('<td/>').append(status)).append($('<td/>').append(item.sensorLocation)));
+                    }
+                }
+            });
+        },
+
+    });
+
+    $("#list-sensor-modal").modal('show');
 
 }
